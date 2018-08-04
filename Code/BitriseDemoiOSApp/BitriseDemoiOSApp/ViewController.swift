444fb9c71703a3a773691ce345fc5984d6f47543
Abhilasha Chumbalkar <Abhilasha.Chumbalkar@rishabhsoft.com>
2018-08-04 11:28:19 +0530

    Added code

---
+++ Code/BitriseDemoiOSApp/BitriseDemoiOSApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BitriseDemoiOSApp
+//
+//  Created by Abhilasha.Chumbalkar on 04/08/18.
+//  Copyright © 2018 Abhilasha Chumbalkar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
